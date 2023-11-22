35de41c1a9d45ccb5120133740254db7969d5239
Hoang Tien Minh <htminh.1001@gmail.com>
2023-11-22 15:53:41 +0700

    fix: update code to use .env

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 8000;
+  const port = process.env.PORT ?? 3000;
 
   app.enableShutdownHooks();
 
--- src/meme/meme.config.ts
+++ src/meme/meme.config.ts
@@ -5,13 +5,16 @@ import {
 } from "@nestjs/common";
 
 export const getMemeUploadOptions = (): ParseFileOptions => {
-  let maxSize = Number(process.env.MAX_MEME_SIZE);
+  let maxSize = Number(process.env.MAX_IMG_SIZE);
   if (isNaN(maxSize)) {
     // Default max image size to 20kB
     maxSize = 20000;
   }
 
-  const validFileType = /\/(jpg|jpeg|png|gif)$/;
+  // Default to support JPG, JPEG, PNG and GIF
+  const validFileType = new RegExp(
+    `\/(${process.env.ALLOW_IMG_TYPE ?? "jpg|jpeg|png|gif"})$`,
+  );
 
   return {
     validators: [

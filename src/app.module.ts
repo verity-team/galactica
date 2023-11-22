5b1cf9e9c714330b162de0c83826e4dc71829ac6
Hoang Tien Minh <htminh.1001@gmail.com>
2023-11-22 16:07:13 +0700

    fix: update configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { MemeModule } from "./meme/meme.module";
 import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import configuration from "@root/config/configuration";
 
 @Module({
   controllers: [AppController],
-  imports: [MemeModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+      load: [configuration],
+      isGlobal: true,
+    }),
+    MemeModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}

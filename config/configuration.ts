584168ff363aee40ca15e56c80ca88a33d7d7a1e
Hoang Tien Minh <htminh.1001@gmail.com>
2023-11-22 16:15:32 +0700

    refactor: remove unused configuration files

--- config/configuration.ts
+++
@@ -1,9 +0,0 @@
-export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
-  image: {
-    // Default maximum upload image size to 20kB
-    maxSize: parseInt(process.env.MAX_IMG_SIZE, 10) || 20000,
-    // Default file support to JPG, JPEG, PNG and GIF
-    validType: process.env.ALLOW_IMG_TYPE || "jpg|jpeg|png|gif",
-  },
-});

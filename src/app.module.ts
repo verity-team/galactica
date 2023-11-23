9d7de574f1bd05e6c6ac56b49f64488f353ab31e
Hoang Tien Minh <htminh.1001@gmail.com>
2023-11-23 15:21:27 +0700

    feat: add Dockerfile to containerize application

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { AppController } from "./app.controller";
 import { MemeModule } from "./meme/meme.module";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
-import configuration from "@root/config/configuration";
 
 @Module({
   controllers: [AppController],
   imports: [
     ConfigModule.forRoot({
       envFilePath: ".env",
-      load: [configuration],
       isGlobal: true,
     }),
     MemeModule,

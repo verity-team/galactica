5ef1196fb9ee10c7f690e33a0bca3bc3190a4a7e
Hoang Tien Minh <htminh.1001@gmail.com>
2023-11-24 14:05:14 +0700

    feat: prevent db connection break server. Add fail-safe logic to database connect

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,19 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
+import { PrismaClientInitializationError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+    } catch (error: any) {
+      if (error instanceof PrismaClientInitializationError) {
+        console.warn("Prisma Client cannot be init. " + error.message);
+        return;
+      }
+
+      console.log("Unknown error. " + JSON.stringify(error));
+    }
   }
 }

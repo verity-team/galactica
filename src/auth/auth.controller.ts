8a36a348573fb9b5db7b36d0b8a5e45f49f0bf77
Hoang Tien Minh <htminh.1001@gmail.com>
2023-11-24 20:07:14 +0700

    feat: add getNonce endpoint for constructing SIWE message

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { GetNonceResponse } from "./auth.type";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Get("nonce")
+  getNonce(): GetNonceResponse {
+    const nonce = this.authService.getNonce();
+    return { nonce };
+  }
 }
---
+++ src/auth/auth.type.ts
@@ -0,0 +1,3 @@
+export interface GetNonceResponse {
+  nonce: string;
+}

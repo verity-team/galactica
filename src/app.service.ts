96a25c0fd660e0a656e05f9dba783f1980b944ce
Hoang Tien Minh <htminh.1001@gmail.com>
2023-11-24 13:53:37 +0700

    feat: update health check with database health check

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,36 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   getLiveStatus(): boolean {
     return true;
   }
 
-  getReadyStatus(): boolean {
+  async getReadyStatus(): Promise<boolean> {
     // TODO: Check APIs' dependencies on database
     // TODO: Check APIs' dependencies on 3rd-party APIs (if any)
+    const isDatabaseReady = await this.getDatabaseReadyStatus();
+
+    if (!isDatabaseReady) {
+      return false;
+    }
+
     return true;
+  }
+
+  private async getDatabaseReadyStatus(): Promise<boolean> {
+    try {
+      const result = await this.prismaService.$queryRaw`SELECT 1`;
+      console.log(result);
+      if (!result) {
+        return false;
+      }
+      return true;
+    } catch {
+      return false;
+    }
   }
 }

ed3f064f38739a80ef97eefbff66be622593e59c
Hoang Tien Minh <htminh.1001@gmail.com>
2023-11-26 15:31:05 +0700

    feat: add utility function to write a file into file system

--- src/utils/fs/fileUtil.ts
+++ src/utils/fs/fileUtil.ts
@@ -1,3 +1,7 @@
+import { access, stat, writeFile } from "fs/promises";
+import { extension } from "mime";
+import { v4 as uuidv4 } from "uuid";
+
 /**
  *
  * @param file
@@ -9,9 +13,38 @@ export async function saveFile(
   destination: string,
   fileId?: string,
 ): Promise<boolean> {
-  console.log(file);
-  console.log(destination);
-  console.log(fileId ?? "");
+  if (fileId == null) {
+    fileId = uuidv4();
+  }
+
+  // Check if destination exist
+  try {
+    await access(destination);
+  } catch (error) {
+    console.warn("Path not exist. Abort current operation");
+    return false;
+  }
+
+  const fileExtension = extension(file.mimetype);
+  const filePath = `${destination}/${fileId}.${fileExtension}`;
+
+  // Check if file exist
+  try {
+    const fileStat = await stat(filePath);
+    if (fileStat) {
+      console.warn("File existed. Abort current operation");
+      return false;
+    }
+  } catch {
+    // Ignore error because it's okay if the file is non-existent
+  }
+
+  try {
+    await writeFile(filePath, file.buffer);
+  } catch (error) {
+    console.warn("Error while writing file", error.message);
+    return false;
+  }
 
   return true;
 }

c4d3eacc3ec04ed50ad7389803178797b6efb24a
Hoang Tien Minh <htminh.1001@gmail.com>
2023-11-29 00:50:57 +0700

    fix: add missing dependencies for tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
+import { PrismaModule } from "@/prisma/prisma.module";
+import { ConfigModule } from "@nestjs/config";
 
 describe("AuthService", () => {
   let service: AuthService;
@@ -7,6 +9,7 @@ describe("AuthService", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService],
+      imports: [PrismaModule, ConfigModule],
     }).compile();
 
     service = module.get<AuthService>(AuthService);

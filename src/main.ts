ba9d889a11136743539478b9f54b37590270226e
Hoang Tien Minh <htminh.1001@gmail.com>
2023-12-04 22:48:52 +0700

    feat: add middleware to log request endpoint and their elapsed time

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { requestLogger } from "./utils/middlewares/logger.middleware";
+import { requestTimer } from "./utils/middlewares/performance.middleware";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { logger: console });
+  const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
+
+  app.use(requestLogger);
+  app.use(requestTimer);
 
   app.enableShutdownHooks();
   app.useGlobalPipes(new ValidationPipe());
